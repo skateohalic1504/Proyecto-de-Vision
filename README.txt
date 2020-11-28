3ba5c3a85e1b23022b7da93d81fb02d94de8f491
skateohalic1504 <jeffreyortizsantana1504@gmail.com>
2020-11-28 19:22:59 -0400

    initial commit

---
+++ README.txt
@@ -0,0 +1,34 @@
+!!!PARA ACLARAR CUALQUIER!!!
+!!!DUDA DE ESTE ENTREGA !!!
+!!!DE PROYECTO!!!
+
+//////////////////////////////////////////////////////////////////////////////
+
+(carpeta) Project:
+PROGRAMA PARA RECONOCER ROSTROS CONOCIDOS
+ELABORADO EN NODE.JS Y FACEAPI.JS
+ASEGURAR TENER LAS DEPENDENCIAS INSTALADAS (NODE.JS Y FACEAPI.JS)
+PARA ABRIR EL PROYECTO
+ESCRIBIR EN EL TERMINAL "run dev start"
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+(CARPETA) project3:
+
+PROGRAMA PARA RECONOCER MANOS Y GESTOS
+ELABORADO EN VANILLA JS Y TENSERFLOW.JS
+ASEGURARSE DE TENER LAS DEPENDENCIAS INSTALADAS (LIBRERIA TENSERFLOW.JS)
+PARA ABRIR EL PROYECTO
+ESCRIBIR EN TERMINAR "npm start"
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+Proposito de hacerlo en dos tecnologias diferentes fue la siguiente: 
+
+- aunque tenserflow tiene la capacidad de reconocer caras, no lo hace lo 
+suficientemente bien como faceapi, de igual manera que faceapi.js no tiene detector 
+de manos ni de gestos.
+
+--opencv fue una opcion, pero el compilador de python aun me sigue dando error!!!
---
+++ project3/src/utilities.js
@@ -0,0 +1,60 @@
+
+//puntas de los dedos
+const fingerJoints ={
+    thumb: [0,1,2,3,4],
+    indexFinger: [0,6,7,8],
+    middleFinger:[0,9,10,11,12],
+    ringFinger:[0,13,14,15,16],
+    pinky:[0,17,18,19,20]
+}
+
+
+//dibujar la mano
+export const drawHand = (predictions, ctx) =>{
+    //tenemos prediccion?
+    if(predictions.length>0){
+        predictions.forEach((prediction) =>{
+            const landmarks = prediction.landmarks;
+
+            //loop entre los dedos
+            for (let j = 0; j<Object.keys(fingerJoints).length; j++) {
+                    let finger = Object.keys(fingerJoints)[j];
+                for(let k =0; k<fingerJoints[finger].length -1; k++){
+
+                    const firstJointIndex = fingerJoints[finger][k];
+                    const secondJointIndex = fingerJoints[finger][k+1];
+
+                    //lineas
+                    ctx.beginPath();    
+                    ctx.moveTo(
+                        landmarks[firstJointIndex][0],
+                        landmarks[firstJointIndex][1]
+                    );
+                    ctx.lineTo(
+                        landmarks[secondJointIndex][0],
+                        landmarks[secondJointIndex][1]
+                    )
+                    ctx.strokeStyle="plum";
+                    ctx.lineWidth = 4;
+                    ctx.stroke();
+                }
+            }
+
+
+
+        
+            for(let i=0; i<landmarks.length; i++){
+                //punto x
+                const x = landmarks[i][0];
+                //punto y
+                const y = landmarks[i][1];
+                //dibujarlos
+                ctx.beginPath();
+                ctx.arc(x,y,5,0,3 * Math.PI);
+                //color de linea
+                ctx.fillStyle = "aqua";
+                ctx.fill();
+            }
+        })
+    }
+}
